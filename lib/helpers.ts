04de111d5b0e5c32745c2ca30ddf790d4ce87148
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2021-07-08 19:00:13 -0400

    add more demographic info to filter

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -77,6 +77,9 @@ export interface Entity {
     ImagingAssayType?: string;
     AssayType?: string;
     Race: string;
+    Ethnicity: string;
+    CountryofResidence: string;
+    Gender: string;
 
     // Derived or attached in frontend
     atlas: Atlas;
